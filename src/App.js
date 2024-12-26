d891ead2893e7714167fe38014dbb8c31d016af1
ParasArya289 <parasarya289@gmail.com>
2024-12-26 20:35:02 +0530

    feat: auto logout

--- src/App.js
+++ src/App.js
@@ -5,10 +5,40 @@ import Login from "./Pages/Login";
 import Dashboard from "./Pages/Dashboard";
 import ProtectedRoute from "./Components/ProtectedRoute";
 import { useEffect } from "react";
+import { useAuth } from "./Context/authContext";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
+  const { token, setToken } = useAuth();
+
   const navigate = useNavigate();
   useEffect(() => navigate("/dashboard"), []);
+
+  useEffect(() => {
+    let interval;
+
+    if (token) {
+      interval = setInterval(() => {
+        const decodedToken = jwtDecode(token); // Decode the token to get exp
+        const expirationTime = decodedToken.exp * 1000 - Date.now();
+
+        if (expirationTime <= 0) {
+          setToken("");
+          localStorage.removeItem("token");
+          alert("Logged out");
+          return () => clearInterval(interval);
+        } else {
+          console.log(
+            "I AM interval: ",
+            Math.floor(expirationTime / (1000 * 60)),
+            ":",
+            Math.floor((expirationTime % (1000 * 60)) / 1000)
+          );
+        }
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [token]);
 
   return (
     <>
